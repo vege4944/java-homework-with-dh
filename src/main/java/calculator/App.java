5562991f4f5be0806f693b535caf8d5eb82ddc23
Joohee Bae <vege4944@gmail.com>
2024-07-29 21:47:30 +0900

    second requirment

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,10 +6,14 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("첫 번째 숫자를 입력하세요: ");//알림을 주는 메세지 역할
         int num1 = sc.nextInt(); //숫자를 입력을 받는 구간
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
+
+        System.out.println("사칙연산 기호를 입력하세요: "); //알림 메세지 주기
+        char operator = sc.next().charAt(0); //사칙연산을 입력받는 구간
 
 
 
