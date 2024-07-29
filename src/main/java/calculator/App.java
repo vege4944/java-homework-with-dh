f29352b65c9fb2494e9340a0e6b9fdf5f1f2688d
Joohee Bae <vege4944@gmail.com>
2024-07-29 21:41:30 +0900

    first requirment

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,23 @@
-package calculator;
+package main.java.calculator;
+
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("첫 번째 숫자를 입력하세요: ");//알림을 주는 메세지 역할
+        int num1 = sc.nextInt(); //숫자를 입력을 받는 구간
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
+
+
+
+
+//        System.out.println("첫 번째 숫자를 입력하세요: ");
+//        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+//        System.out.println("두 번째 숫자를 입력하세요: ");
+//        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
     }
 }
